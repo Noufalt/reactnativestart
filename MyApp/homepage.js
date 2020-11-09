a7214fa7b611f82ff76840be211528e20150337f
Noufalt <thazhathethiln@aecl.com>
2020-11-09 11:17:01 +0300

    Api integration using axios

--- MyApp/homepage.js
+++ MyApp/homepage.js
@@ -1,62 +1,87 @@
 // Homescreen.js
+import axios from 'axios';
+import qs from 'qs';
 import React, {Component} from 'react';
-import {StyleSheet,Button, View,Image, Text,Touchable, Platform} from 'react-native';
-import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import {
+  StyleSheet,
+  Button,
+  View,
+  Image,
+  Text,
+  Touchable,
+  Platform,
+} from 'react-native';
+import {TextInput, TouchableOpacity} from 'react-native-gesture-handler';
 // import TextInputWithValidation from 'react-native-input-validator';
 
-import { color } from 'react-native-reanimated';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import {color} from 'react-native-reanimated';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 export default class Homescreen extends Component {
-  constructor(props) 
-  {
+  constructor(props) {
     super(props);
     this.state = {
-      userName: "",
-      password:"",
+      userName: '',
+      password: '',
     };
   }
 
-   postCallResponse= ()  => 
-     {
-       console.log('UN:'+this.state.userName);
-       console.log('PWD:'+this.state.password);
-        fetch('https://amneen-test.azurewebsites.net/Token',{
-              method:'POST',
-              headers:
-                {
-                  'Accept': 'application/json',
-                  'Content-Type': 'application/json'
-                },
-                body:JSON.stringify(
-                  {
-                    'grant_type':'password',
-                    'username':this.state.userName,
-                    'password':this.state.password
-                  })
-                  .then((response) => response.json())
-                  .then((response)=>
-                    {
-                      //console.log(response.content);
-                      console.log(response);
-                    })
-                })
-                .catch(function (error) {
-                  console.log(error);
-                });
-  }
+  postCallResponse = () => {
+    console.log('***********************************************');
+    console.log('UN:' + this.state.userName);
+    console.log('PWD:' + this.state.password);
+    var data = qs.stringify({
+      grant_type: 'password',
+      username: this.state.userName,
+      password: this.state.password,
+    });
+    var config = {
+      method: 'post',
+      url: 'https://amneen-test.azurewebsites.net/Token',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      data: data,
+    };
 
-  async OnButtonPressed() 
-  {
-    
-    var uName=this.state.userName;
-    var uPass=this.state.password;
+    axios(config)
+      .then(function (response) {
+        console.log('******************************************');
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 
+    // fetch('https://amneen-test.azurewebsites.net/Token', {
+    //   method: 'POST',
+    //   headers: {
+    //     Accept: 'application/json',
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     grant_type: 'password',
+    //     username: this.state.userName,
+    //     password: this.state.password,
+    //   })
+    //     .then((response) => response.json())
+    //     .then((response) => {
+    //       //console.log(response.content);
+    //       console.log(response);
+    //     }),
+    // }).catch(function (error) {
+    //   console.log(error);
+    // });
+  };
 
-    console.log('UN:'+this.state.userName);
-       console.log('PWD:'+this.state.password);
+  async OnButtonPressed() {
+    var uName = this.state.userName;
+    var uPass = this.state.password;
 
-    console.log("On Button Pressed Hit");
+    console.log('UN:' + this.state.userName);
+    console.log('PWD:' + this.state.password);
+
+    console.log('On Button Pressed Hit');
 
     this.postCallResponse();
     //var res1=await checkIfUserNameIsValid();
@@ -64,21 +89,16 @@ export default class Homescreen extends Component {
     //console.log('Response json : '+responseJson)
     //return responseJson;
 
-    if(uName.trim()=='Nouf' && uPass.trim()=="Password1")
-    {
+    if (uName.trim() == 'Nouf' && uPass.trim() == 'Password1') {
       this.props.navigation.navigate('Profile', {name: 'Jane'});
-    }
-    else
-      alert('Invalid username or password');
+    } else alert('Invalid username or password');
   }
 
-  async checkIfUserNameIsValid()
-  {
-    const response=await fetch('https://reqres.in/api/users?page=2');
-    const responseJson=await response.json();
-    console.log('Nouf'+responseJson);
+  async checkIfUserNameIsValid() {
+    const response = await fetch('https://reqres.in/api/users?page=2');
+    const responseJson = await response.json();
+    console.log('Nouf' + responseJson);
     return responseJson;
-
 
     // let response=await fetch('https://amneen-test.azurewebsites.net/Token',
     // {
@@ -99,21 +119,18 @@ export default class Homescreen extends Component {
 
     //let responseJson=await response.json();
     //return responseJson;
-
-
   }
 
+  //}
 
-//}
-
-//const HomeScreen = ({navigation}) => {
-render() {
-  return (
-    <View style={style.container} >
-      {/* <Image source={require('./icons/headerPanel.png')} style={style.imageStyle}/> */}
-      <View style={style.subcontainer}>
-      <Text style={style.labelStyle}>User name</Text>
-      {/* <TextInputWithValidation
+  //const HomeScreen = ({navigation}) => {
+  render() {
+    return (
+      <View style={style.container}>
+        {/* <Image source={require('./icons/headerPanel.png')} style={style.imageStyle}/> */}
+        <View style={style.subcontainer}>
+          <Text style={style.labelStyle}>User name</Text>
+          {/* <TextInputWithValidation
         onRef={(r) => {
             this.input = r;
           }} type='email'
@@ -123,87 +140,82 @@ render() {
           placeholder="User Name">
           <Text>Default</Text>
       </TextInputWithValidation> */}
-      <TextInput
-        onChangeText={(text)=>this.setState({userName:text})} 
-        style={style.entryStyle} placeholder="User name"/>
-      <Text style={style.labelStyle}>Password</Text>
-      <TextInput 
-      onChangeText={(text)=>this.setState({password:text})}
-      secureTextEntry={true} style={style.entryStyle} placeholder="Password"/>
-      {/* <Button
+          <TextInput
+            onChangeText={(text) => this.setState({userName: text})}
+            style={style.entryStyle}
+            placeholder="User name"
+          />
+          <Text style={style.labelStyle}>Password</Text>
+          <TextInput
+            onChangeText={(text) => this.setState({password: text})}
+            secureTextEntry={true}
+            style={style.entryStyle}
+            placeholder="Password"
+          />
+          {/* <Button
         title="Login"
         onPress={() => navigation.navigate('Profile', {name: 'Jane'})}/> */}
-        <TouchableOpacity
-          style={style.buttonStyle}
-          onPress={async() => await this.OnButtonPressed()}
-          underlayColor='#fff'>
+          <TouchableOpacity
+            style={style.buttonStyle}
+            onPress={async () => await this.OnButtonPressed()}
+            underlayColor="#fff">
             <Text style={style.textButtonStyle}>Login</Text>
-        </TouchableOpacity>
+          </TouchableOpacity>
         </View>
-    </View>
-  );
-}
-}
-
-const style=StyleSheet.create
-(
-  {
-    container: 
-    {
-      flex: 1,
-      backgroundColor:'white'
-    },
-    subcontainer: {
-      flex: 1,
-      marginTop:50,
-      marginLeft:10,
-      marginRight:10,
-      backgroundColor:'white'
-    },
-    labelStyle:
-    {
-      marginBottom:10,
-      color:'blue',
-      fontWeight:'bold',
-      fontSize:20
-    },
-
-    entryStyle:
-    {
-      marginBottom:10,
-      height:40,
-      borderColor:'gray',
-      borderWidth:1,
-      borderRadius:8
-    },
-
-    buttonStyle:
-    {
-      marginRight:0,
-      marginLeft:0,
-      marginTop:10,
-      paddingTop:10,
-      paddingBottom:10,
-      backgroundColor:'#1E6738',
-      borderRadius:10,
-      borderWidth: 1,
-      borderColor: '#fff'
-    },
-
-    textButtonStyle:
-    {
-      color:'#fff',
-      textAlign:'center',
-      paddingLeft :10,
-      paddingRight : 10
-    },
-
-    imageStyle:
-    {
-      height:180
-    }
+      </View>
+    );
   }
-)
+}
 
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  subcontainer: {
+    flex: 1,
+    marginTop: 50,
+    marginLeft: 10,
+    marginRight: 10,
+    backgroundColor: 'white',
+  },
+  labelStyle: {
+    marginBottom: 10,
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+
+  entryStyle: {
+    marginBottom: 10,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 8,
+  },
+
+  buttonStyle: {
+    marginRight: 0,
+    marginLeft: 0,
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: '#1E6738',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#fff',
+  },
+
+  textButtonStyle: {
+    color: '#fff',
+    textAlign: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+
+  imageStyle: {
+    height: 180,
+  },
+});
 
 //export default HomeScreen;
